0c52a55703f101283cb5e60a1d9444fd8ae9a0f3
Andrew DIVittorio <18adivitto@gmail.com>
2019-12-23 21:16:00 -0500

    Initial project loaded

---
+++ REWORKED RHYTHM SYSTEM TESTING/Assets/CODE/Player Controls/PlayerBeatDetection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBeatDetection : MonoBehaviour
+{
+    BeatTracker BT;
+    float buffer = .2f;
+
+    public GameObject pass;
+    public GameObject fail;
+
+    public bool punching = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BT = GameObject.Find("MusicManager").GetComponent<BeatTracker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if ((BT.songPosInBeats > (Mathf.Round(BT.songPosInBeats) - buffer)) && (BT.songPosInBeats < (Mathf.Round(BT.songPosInBeats) + buffer)))
+            {
+                //punching = true;
+                //Debug.Log("PASS");
+                Instantiate(pass, this.gameObject.transform);
+            }
+            else
+            {
+                //punching = false;
+                //Debug.Log("FAIL");
+                Instantiate(fail, this.gameObject.transform);
+            }
+           // Debug.Log(BT.songPosInBeats - Mathf.Round(BT.songPosInBeats));
+        }
+    }
+
+
+}
---
+++ REWORKED RHYTHM SYSTEM TESTING/Assets/CODE/Player Controls/PlayerMovement.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+     float speed = 3.5f;
+     float jumpSpeed = 11.0f;
+     float gravity = 40.0f;
+
+    public Vector3 moveDirection = Vector3.zero;
+
+    CameraMovement CamData;
+    PlayerAnimations AnimData;
+
+    bool touchingController;
+    bool onRight;
+    bool slidingOff;
+    float slideDirection;
+
+    BoxCollider boxColl;
+
+    public bool crouching = false;
+
+    void Start()
+    {
+        CamData = GameObject.Find("Main Camera").GetComponent<CameraMovement>();
+        AnimData = GetComponent<PlayerAnimations>();
+        characterController = GetComponent<CharacterController>();
+        boxColl = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        RelativePositionCalc();
+        ColliderPos();
+        if (characterController.isGrounded)
+        {
+
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0);
+            moveDirection *= speed;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            slidingOff = false;
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                crouching = true;
+            }
+            else
+            {
+                crouching = false;
+            }
+        }
+
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        if (this.gameObject.name[0] == '0') //FOR DEBUGGING ONLY
+        {
+            if (crouching || AnimData.punching)
+            {
+                moveDirection.x = 0;
+            }
+        }
+        else
+        {
+            moveDirection.x = 0;
+            moveDirection.y = -1;
+        }
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+    void ColliderPos()
+    {
+        if (onRight)
+        {
+            boxColl.center = new Vector3(-7, 2, 0);
+        }
+        else
+        {
+            boxColl.center = new Vector3(7, 2, 0);
+        }
+    }
+    void RelativePositionCalc()
+    {
+        if (AnimData.otherPlayer.position.x > transform.position.x)
+        {
+            onRight = false;
+            slideDirection = -1;
+        }
+        else 
+        {
+            onRight = true;
+            slideDirection = 1;
+        }
+    }
+}
